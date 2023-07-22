1881831648c49fb61f22a0f16c4a5c8cdadcc1d1
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2023-07-22 18:22:10 -0300

    adding validation for user id

--- api/app/Http/Controllers/CurrentWeatherController.php
+++ api/app/Http/Controllers/CurrentWeatherController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CurrentWeatherController extends Controller
 {
 
     public function show(Request $request, int $userId)
     {
+        $validator = Validator::make(['user_id' => $userId], [
+            'user_id' => ['required', 'exists:users,id'],
+        ])->stopOnFirstFailure(true);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->all(),
+            ], 404);
+        }
+
+
         return response()->json([
             'success' => true,
             'id' => $userId,
